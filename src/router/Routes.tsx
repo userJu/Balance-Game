b175b9d53e67929621b501748efeb27f22b5440e
userJu <boochickenood@gmail.com>
2022-11-10 16:33:51 +0900

    Fix: modify route element
    Routes.tsx 잘못 설정된 Route element를 수정

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import NotFoundPage from "../pages/NotFoundPage";
+import PlayingGamePage from "../pages/PlayingGamePage";
+import WaitGamePage from "../pages/WaitGamePage";
 import { MAIN_PAGE, PLAYING_GAME_PAGE, WAIT_GAME_PAGE } from "./routePath";
 
 const Router = () => {
   return (
     <Routes>
       <Route path={MAIN_PAGE} element={<MainPage />} />
-      <Route path={WAIT_GAME_PAGE} element={<MainPage />} />
-      <Route path={PLAYING_GAME_PAGE} element={<MainPage />} />
+      <Route path={WAIT_GAME_PAGE} element={<WaitGamePage />} />
+      <Route path={PLAYING_GAME_PAGE} element={<PlayingGamePage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
