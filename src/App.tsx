cea1811d913f7201736eef9d094aab078d976795
Juhyun <boochickenood@gmail.com>
2022-11-20 14:28:29 +0900

    Feat: UserCard componnet
    게임 준비 화면에서 user의 상태를 보여주는 userCard 컴포넌트 제작

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Container from "./components/Container";
 import GameCard from "./components/GameCard";
 import Input from "./components/Input";
 import SmallButton from "./components/SmallButton";
+import UserCard from "./components/UserCard";
 
 function App() {
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,6 +16,7 @@ function App() {
       <Input onChange={onInputChange} />
       <Container>
         <div>hello i'm container</div>
+        <UserCard></UserCard>
         <GameCard></GameCard>
         <SmallButton>▶</SmallButton>
       </Container>
