eec315c4a03dead310173ef15bbdc37047d1c082
Juhyun <boochickenood@gmail.com>
2022-11-30 19:38:18 +0900

    Feat: mainpage 기본 출력

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { gameList } from "../services/mock/gameList";
+import { rankingList } from "../services/mock/ranking";
 
 const MainPage = () => {
   return (
     <div>
-      <h1>MainPage</h1>
+      <h1>Balance</h1>
+      <div>user 정보</div>
+      <button>바로 시작</button>
+      <button>방 만들기</button>
+
+      <ul>
+        {gameList.map((game) => (
+          <li>{game.title}</li>
+        ))}
+      </ul>
+      <h3>ranking</h3>
+      {/* 랭킹이 없을때는 일단 공란으로 내비두기  */}
+      <ul>
+        {rankingList.map((rank) => (
+          <li>rank.userId</li>
+        ))}
+      </ul>
     </div>
   );
 };
