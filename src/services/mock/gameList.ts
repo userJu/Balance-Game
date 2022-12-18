dcc6dfd2c6aa3429346e44ca0a4d0ceb5de1497a
Juhyun <boochickenood@gmail.com>
2022-12-18 19:58:40 +0900

    Fix: GameList에 Gamer 데이터 List 형식으로 추가

---
+++ src/services/mock/gameList.ts
@@ -0,0 +1,23 @@
+import { Game } from "../../entities/game";
+import { gamers } from "./gamer";
+
+export const gameList: Game[] = [
+  {
+    gameId: "1",
+    title: "사랑 우정 벨겜 ㄱㄱ",
+    owner: "캐슈",
+    topic: [3, 4],
+    max_members: 8,
+    members: gamers,
+    createDate: new Date(),
+  },
+  {
+    gameId: "2",
+    title: "사랑 기타 벨겜 ㄱㄱ2",
+    owner: "캐슈2",
+    topic: [3, 5],
+    max_members: 5,
+    members: gamers,
+    createDate: new Date(),
+  },
+];
