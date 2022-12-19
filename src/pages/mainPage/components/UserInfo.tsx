eb86c9e1302003386884a38e8daf93f6a59b8f53
Juhyun <boochickenood@gmail.com>
2022-12-19 15:02:37 +0900

    Feat: UserInfo 컴포넌트 분리

---
+++ src/pages/mainPage/components/UserInfo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserCard from "../../../components/UserCard";
+import { gamer } from "../../../services/mock/gamer";
+
+const UserInfo = () => {
+  return (
+    <div>
+      <h3>user 정보</h3>
+      <UserCard gamer={gamer}></UserCard>
+    </div>
+  );
+};
+
+export default UserInfo;
