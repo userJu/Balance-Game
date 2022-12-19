08cae2a641d6fbdba32ab14db0228f672afc4c74
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-12-19 23:35:09 +0900

    Feat: Container에 styled-components prop 추가
    flex를 이용하기 위해 grow, shrink, basis prop을 추가하고
    styled component에 넘겨줘서 사용할 수 있도록 했다

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,21 +2,44 @@ import React, { ReactNode } from "react";
 import styled from "styled-components";
 import { colors } from "../style/styles";
 
-const Div = styled.div`
+interface Flex {
+  grow?: number;
+  shrink?: number;
+  basis: number | string;
+}
+
+const Div = styled.div<Flex>`
   background-color: ${colors.gray0};
   width: 95%;
-  height: 50vw;
+  min-height: 50vw;
+
   margin: auto;
   margin-top: 3rem;
   margin-bottom: 3rem;
+  border: 1px solid pink;
+  flex-grow: ${(props) => props.grow};
+  flex-shrink: ${(props) => props.shrink};
+  flex-basis: ${(props) => props.basis};
 `;
 
 interface PropsContainer {
   children: ReactNode;
+  grow?: number;
+  shrink?: number;
+  basis?: number | string;
 }
 
-const Container = ({ children }: PropsContainer) => {
-  return <Div>{children}</Div>;
+const Container = ({
+  children,
+  grow = 1,
+  shrink = 1,
+  basis = 1,
+}: PropsContainer) => {
+  return (
+    <Div grow={grow} shrink={shrink} basis={basis}>
+      {children}
+    </Div>
+  );
 };
 
 export default Container;
