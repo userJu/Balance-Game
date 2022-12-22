c4cd9a444e349562f229cc474b1af7d5edde76ee
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-12-22 13:04:20 +0900

    Feat: 사용자가 없을 때 Auth 컴포넌트 출력

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Container from "../../components/Container";
+import { Gamer } from "../../entities/gamer";
+import Auth from "./components/Auth";
 import GameListGrid from "./components/GameListGrid";
 import Lanking from "./components/Lanking";
 import UserInfo from "./components/UserInfo";
@@ -12,12 +14,14 @@ const PageContainer = styled.div`
 `;
 
 const MainPage = () => {
+  const [user, setUser] = useState<null | Gamer>(null);
+
   return (
     <div>
       <h1>Balance</h1>
       <PageContainer>
         <Container basis={"15%"}>
-          <UserInfo></UserInfo>
+          {user !== null ? <UserInfo></UserInfo> : <Auth></Auth>}
         </Container>
 
         <Container basis={"70%"}>
