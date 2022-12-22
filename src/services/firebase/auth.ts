df3cf6f4d8e7205f958733468c9ad23ebfef4ddd
Juhyun <boochickenood@gmail.com>
2022-12-22 19:05:58 +0900

    Feat: Firebase login 함수 제작
    기존 사용자 로그인
    & SignupType interface를 AuthType interface로 이름 변경

--- src/services/firebase/auth.ts
+++ src/services/firebase/auth.ts
@@ -1,11 +1,14 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "./initializer";
-interface SignupType {
+interface AuthType {
   email: string;
   password: string;
 }
 
-export const signup = ({ email, password }: SignupType) => {
+export const signup = ({ email, password }: AuthType) => {
   // const auth = getAuth();
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
@@ -18,6 +21,21 @@ export const signup = ({ email, password }: SignupType) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       // ..
+      console.log(errorCode, errorMessage);
+    });
+};
+
+export const login = ({ email, password }: AuthType) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
       console.log(errorCode, errorMessage);
     });
 };
