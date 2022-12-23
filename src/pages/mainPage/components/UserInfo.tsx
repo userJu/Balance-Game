2f24b73e7a5829aad3c0c0001917adc12e8afa29
Juhyun <boochickenood@gmail.com>
2022-12-23 14:18:21 +0900

    Feat: LogoutButton 추가
    버튼을 누르면 auth.ts에 작성된 logout 함수 호출

--- src/pages/mainPage/components/UserInfo.tsx
+++ src/pages/mainPage/components/UserInfo.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import UserCard from "../../../components/UserCard";
+import { logout } from "../../../services/firebase/auth";
 import { gamer } from "../../../services/mock/gamer";
 
-const UserInfo = () => {
+interface UserInfoProps {
+  setUserLogout: () => void;
+}
+
+const UserInfo = ({ setUserLogout }: UserInfoProps) => {
+  const onLogoutClick = () => {
+    logout();
+    setUserLogout();
+  };
+
   return (
     <div>
       <h3>user 정보</h3>
       <UserCard {...gamer}></UserCard>
+      <button onClick={onLogoutClick}>로그아웃</button>
     </div>
   );
 };
