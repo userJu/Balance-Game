4e8df46abc5268d2b3ca5e86cdd1aa6a6c83bb67
Juhyun <boochickenood@gmail.com>
2022-12-23 14:08:46 +0900

    Feat: User의 존재여부를 확인할 수 있는 checkAuthState 함수 제작
    onAuthStateChanged라는 firebase 제공 함수를 이용하였다

--- src/services/firebase/auth.ts
+++ src/services/firebase/auth.ts
@@ -70,3 +70,20 @@ export const updateUserProfile = (displayName: string) => {
         // ...
       });
 };
+
+export const checkAuthState = (getUserInfo: (user: User) => void) => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      console.log(user);
+      getUserInfo(user);
+      // ...
+    } else {
+      console.log("유저 없음");
+      // User is signed out
+      // ...
+    }
+  });
+};
