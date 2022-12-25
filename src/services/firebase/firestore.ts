0a31b36a4c9b74c20a918166148871afc73fd83c
Juhyun <boochickenood@gmail.com>
2022-12-25 16:08:11 +0900

    Feat: 저장되어있는 UserObj 불러오는 getUserObject 함수 제작

--- src/services/firebase/firestore.ts
+++ src/services/firebase/firestore.ts
@@ -1,4 +1,4 @@
-import { doc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { FIRESTORE_COLLECTIONS } from "../../constants/firestore";
 import { Gamer } from "../../entities/gamer";
 import { db } from "./initializer";
@@ -6,4 +6,18 @@ import { db } from "./initializer";
 export const setNewUser = async (uid: string, gamer: Gamer) => {
   console.log(uid, gamer);
   await setDoc(doc(db, FIRESTORE_COLLECTIONS.users, uid), gamer);
+};
+
+export const getUserObject = async (uid: string) => {
+  const docRef = doc(db, "users", uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+    return docSnap.data();
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+    return null;
+  }
 };
